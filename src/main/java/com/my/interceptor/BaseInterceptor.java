b9ebaacd9b1f783bb6ba454e910db56a11293137
blackjack <2637201260@qq.com>
2019-07-21 16:39:40 +0800

    拦截基类

---
+++ src/main/java/com/my/interceptor/BaseInterceptor.java
@@ -0,0 +1,30 @@
+package com.my.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * create by blackjack on 2019/6/21
+ */
+@Component
+public class BaseInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("前置拦截");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+      System.out.println("后置拦截");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
